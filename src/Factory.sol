ed53ee599f087a657e36e30a781ff8b4579684b3
SHAKOTN <shakotn@protonmail.com>
2023-10-03 11:41:36 +0300

    feat: WIP on vester

---
+++ src/Factory.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "../lib/openzeppelin-contracts/contracts/proxy/Clones.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract Factory is Ownable {
+    using Clones for address;
+    //////////////////////////////////////////////////////////////////
+    //                         Constants                            //
+    //////////////////////////////////////////////////////////////////
+    address public constant DAO_MSIG = address(1337);
+
+    //////////////////////////////////////////////////////////////////
+    //                         Storage                              //
+    //////////////////////////////////////////////////////////////////
+    address public implementation;
+
+    //////////////////////////////////////////////////////////////////
+    //                         Events                               //
+    //////////////////////////////////////////////////////////////////
+    event LogImplementationChanged(
+        address indexed oldImplementation,
+        address indexed newImplementation
+    );
+
+    /// @notice Factory constructor
+    /// @param _implementation Address of the implementation
+    constructor(address _implementation) Ownable() {
+        address oldImplementation = implementation;
+        implementation = _implementation;
+        emit LogImplementationChanged(
+            oldImplementation,
+            _implementation
+        );
+        // Transfer ownership of this contract to DAO multisig
+        transferOwnership(DAO_MSIG);
+    }
+
+    /// @notice Set implementation address
+    /// @param _implementation Address of the implementation
+    function setImplementation(address _implementation) public onlyOwner {
+        implementation = _implementation;
+        emit LogImplementationChanged(
+            implementation,
+            _implementation
+        );
+    }
+
+    /// @notice Get implementation address
+    function getImplementation() public view returns (address) {
+        return implementation;
+    }
+
+    /// @notice Deploy a new vesting contract
+    function deployVestingContract(
+        address _owner
+    ) public onlyOwner returns (address vestingContract) {
+        vestingContract = implementation.clone();
+    }
+}
---
+++ src/Vester.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "../lib/openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract Vester is Initializable, Ownable {
+    function initialise() public initializer {
+        // Do something
+    }
+}
