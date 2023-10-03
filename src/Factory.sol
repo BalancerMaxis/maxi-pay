c2b5cbd36162259415047276671ffa6559d940b1
SHAKOTN <shakotn@protonmail.com>
2023-10-03 11:58:47 +0300

    feat: adding contract skeletons for vester

--- src/Factory.sol
+++ src/Factory.sol
@@ -3,12 +3,14 @@ pragma solidity 0.8.20;
 
 import "../lib/openzeppelin-contracts/contracts/proxy/Clones.sol";
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {Vester} from "./Vester.sol";
 
 contract Factory is Ownable {
     using Clones for address;
     //////////////////////////////////////////////////////////////////
     //                         Constants                            //
     //////////////////////////////////////////////////////////////////
+
     address public constant DAO_MSIG = address(1337);
 
     //////////////////////////////////////////////////////////////////
@@ -19,20 +21,15 @@ contract Factory is Ownable {
     //////////////////////////////////////////////////////////////////
     //                         Events                               //
     //////////////////////////////////////////////////////////////////
-    event LogImplementationChanged(
-        address indexed oldImplementation,
-        address indexed newImplementation
-    );
+    event LogImplementationChanged(address indexed oldImplementation, address indexed newImplementation);
+    event LogVestingContractDeployed(address indexed vestingContract, address indexed owner);
 
     /// @notice Factory constructor
     /// @param _implementation Address of the implementation
     constructor(address _implementation) Ownable() {
         address oldImplementation = implementation;
         implementation = _implementation;
-        emit LogImplementationChanged(
-            oldImplementation,
-            _implementation
-        );
+        emit LogImplementationChanged(oldImplementation, _implementation);
         // Transfer ownership of this contract to DAO multisig
         transferOwnership(DAO_MSIG);
     }
@@ -41,10 +38,7 @@ contract Factory is Ownable {
     /// @param _implementation Address of the implementation
     function setImplementation(address _implementation) public onlyOwner {
         implementation = _implementation;
-        emit LogImplementationChanged(
-            implementation,
-            _implementation
-        );
+        emit LogImplementationChanged(implementation, _implementation);
     }
 
     /// @notice Get implementation address
@@ -53,9 +47,9 @@ contract Factory is Ownable {
     }
 
     /// @notice Deploy a new vesting contract
-    function deployVestingContract(
-        address _owner
-    ) public onlyOwner returns (address vestingContract) {
+    function deployVestingContract(address _owner) public onlyOwner returns (address vestingContract) {
         vestingContract = implementation.clone();
+        Vester(vestingContract).initialise(_owner, DAO_MSIG);
+        emit LogVestingContractDeployed(vestingContract, _owner);
     }
 }
--- src/Vester.sol
+++ src/Vester.sol
@@ -2,10 +2,45 @@
 pragma solidity 0.8.20;
 
 import "../lib/openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
-import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract Vester is Initializable, Ownable {
-    function initialise() public initializer {
-        // Do something
+library VesterErrors {
+    error NotDaoMsig();
+    error NotBeneficiary();
+}
+
+contract Vester is Initializable {
+    //////////////////////////////////////////////////////////////////
+    //                         Constants                            //
+    //////////////////////////////////////////////////////////////////
+    address public constant STAKED_AURABAL = address(0x95c1D2014909c04202fa73820B894b45F054F25e);
+    //////////////////////////////////////////////////////////////////
+    //                         Storage                              //
+    //////////////////////////////////////////////////////////////////
+    address public beneficiary;
+    address public daoMsig;
+
+    //////////////////////////////////////////////////////////////////
+    //                         Events                               //
+    //////////////////////////////////////////////////////////////////
+    event BeneficiaryChanged(address indexed oldBeneficiary, address indexed newBeneficiary);
+
+    /// @notice Contract initializer
+    /// @param _beneficiary Address of the beneficiary that will be able to claim tokens
+    /// @param _daoMsig Address of the DAO multisig
+    function initialise(address _beneficiary, address _daoMsig) public initializer {
+        beneficiary = _beneficiary;
+        daoMsig = _daoMsig;
+    }
+
+    //////////////////////////////////////////////////////////////////
+    //                   Permissioned Setters                       //
+    //////////////////////////////////////////////////////////////////
+    function setBeneficiary(address _beneficiary) public {
+        if (msg.sender != daoMsig) {
+            revert VesterErrors.NotDaoMsig();
+        }
+        address oldBeneficiary = beneficiary;
+        beneficiary = _beneficiary;
+        emit BeneficiaryChanged(oldBeneficiary, _beneficiary);
     }
 }
